f5a8422b6fcec7c7db25303b32d09c55a29ac760
Francis S. Crisostomo <francis.s.crisostomo@gmail.com>
2024-09-13 17:50:28 +0800

    Add Routes & Middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         //
+        $middleware->alias([
+            'custom.auth' => \App\Http\Middleware\CustomAuth::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (AuthenticationException $e, Request $request) {
